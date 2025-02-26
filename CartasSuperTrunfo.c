72255475f56f504d4d2861de7f91d3206ab7eb05
Luiz Danillo Morais <144749590+Danillo-anjos@users.noreply.github.com>
2025-02-26 20:03:30 +0000

    Desafio completo com comentários inseridos

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,13 +11,13 @@ int main() {
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
     //Declaração de variáveis para a primeira carta
-    char estado1;
-    char codigo1[4];
-    char nomeCidade1[50];
-    int populacao1;
-    float area1;
-    float pib1;
-    int pontosTuristicos1;
+    char estado1;           //char = variável que armazena caracteres. Usada aqui para armazenar a letra do estado (A-H).
+    char codigo1[4];        //Armazena o código da carta (ex: "A01").
+    char nomeCidade1[50];   //Armazena o nome da cidade (Até 49 caracteres + '\0' no final).
+    int populacao1;         //int = variável que armazena números inteiros. Usada aqui para armazenar o número de habitantes da cidade.
+    float area1;            //float = variável que armazena pontos flutuantes, ou seja, casas decimais. Usada aqui para armazenar a área da cidade em Km²
+    float pib1;             //Armazenar o PIB da cidade em bilhões de reais.
+    int pontosTuristicos1;  //Armazenar o número de pontos turísticos.
 
     //Declaração de variáveis para a segunda carta
     char estado2;
@@ -31,28 +31,28 @@ int main() {
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    printf("\nDigite os dados da Carta 1:\n");
+    printf("\nDigite os dados da Carta 1:\n");          //printf = Sintaxe padrão de leitura.
 
     printf("Estado (A-H): ");
-    scanf("%c", &estado1);
+    scanf("%c", &estado1);                              //scanf = Sintaxe padrão para capturar os dados digitados pelo usuário. %c lê um caractere representando o Estado.
 
     printf("Código da carta (ex: A01, B03): ");
-    scanf("%s", codigo1);
+    scanf("%s", codigo1);                               //%s lê uma string contendo o código da carta.
 
     printf("Nome da cidade: ");
-    scanf(" %[^\n]", nomeCidade1);
+    scanf(" %[^\n]", nomeCidade1);                      //%[^\n] lê uma string com espaços até a quebra de linha.
 
     printf("População: ");
-    scanf("%d", &populacao1);
+    scanf("%d", &populacao1);                           //%d lê um número inteiro representando a população.
 
     printf("Área (em km²): ");
-    scanf("%f", &area1);
+    scanf("%f", &area1);                                //%f lê um número decimal representando a área.
 
     printf("PIB (em bilhões de reais): ");
-    scanf("%f", &pib1);
+    scanf("%f", &pib1);                                 //%f lê um número decimal representando o PIB.
 
     printf("Números de Pontos Turísticos: ");
-    scanf("%d", &pontosTuristicos1);
+    scanf("%d", &pontosTuristicos1);                    //%d lê número inteiro representando os pontos turísticos.
 
     //Leitura de dados da segunda carta
 
@@ -87,8 +87,8 @@ int main() {
     printf("Código: %s\n", codigo1);
     printf("Nome da Cidade: %s\n", nomeCidade1);
     printf("População: %d\n", populacao1);
-    printf("Área: %2.f km²\n", area1);
-    printf("PIB: %.2f bilhões de reais\n", pib1);
+    printf("Área: %2.f km²\n", area1);                              //%.2f Exibe a área com duas casas decimais.
+    printf("PIB: %.2f bilhões de reais\n", pib1);                   //%.2f Exibe o PIB com duas casas decimais.
     printf("Número de pontos Turísticos: %d\n", pontosTuristicos1);
 
     //Exibição de dados da segunda carta
